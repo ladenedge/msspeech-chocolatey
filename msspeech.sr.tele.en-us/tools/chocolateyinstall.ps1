f8acb982bc19df0557ed602f16d58dbefa673fbf
Jay Miller <jay.miller@verint.com>
2019-12-18 09:47:46 -0700

    Package specs and install scripts.

---
+++ msspeech.sr.tele.en-us/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$url        = 'https://download.microsoft.com/download/4/0/D/40D6347A-AFA5-417D-A9BB-173D937BEED4/MSSpeech_SR_en-US_TELE.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url           = $url
+  softwareName  = 'Microsoft Server Speech Recognition Language - TELE (en-US)'
+  checksum      = '854DB2D4CF44DD94D1F6E62659F3C08D695EF13F7C2D599D9825B0305FB54227'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ msspeech/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$url        = 'https://download.microsoft.com/download/A/6/4/A64012D6-D56F-4E58-85E3-531E56ABC0E6/x86_SpeechPlatformRuntime/SpeechPlatformRuntime.msi'
+$url64      = 'https://download.microsoft.com/download/A/6/4/A64012D6-D56F-4E58-85E3-531E56ABC0E6/x64_SpeechPlatformRuntime/SpeechPlatformRuntime.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+  softwareName  = 'Microsoft Server Speech Platform Runtime*'
+  checksum      = '9B42EEC0DB1DC3CB92FCDD795EA0771EDAC1CF58118D12A810810268ADB954B4'
+  checksumType  = 'sha256'
+  checksum64    = '9C0A1D096E80F2AE77D5FC00B5D1A43B665C28EC6196911DE3DCE6D74C115137'
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs

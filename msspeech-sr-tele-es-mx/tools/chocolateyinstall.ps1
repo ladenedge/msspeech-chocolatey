802d0de6b0cbeed9bef2455b8af61daba0ce9614
Jay Miller <jnmiller@cryptofreak.org>
2020-06-08 17:54:38 -0600

    Added new language models.

---
+++ msspeech-sr-tele-es-mx/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$url        = 'https://download.microsoft.com/download/4/0/D/40D6347A-AFA5-417D-A9BB-173D937BEED4/MSSpeech_SR_es-MX_TELE.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url           = $url
+  softwareName  = 'Microsoft Server Speech Recognition Language - TELE (es-MX)'
+  checksum      = '263476526C9BA9F7B07D5624D67BBF4365F6330BCED6EC92F01A89A3241C60FA'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641, 1603)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ msspeech-sr-tele-ja-jp/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$url        = 'https://download.microsoft.com/download/4/0/D/40D6347A-AFA5-417D-A9BB-173D937BEED4/MSSpeech_SR_ja-JP_TELE.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi'
+  url           = $url
+  softwareName  = 'Microsoft Server Speech Recognition Language - TELE (ja-JP)'
+  checksum      = 'B7D352051957A2162AAFA999DAA9549E3F87991D48B88F44D35EE732D2A6288B'
+  checksumType  = 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641, 1603)
+}
+
+Install-ChocolateyPackage @packageArgs
